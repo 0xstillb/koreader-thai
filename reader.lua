0xstillb/koreader-thai

commit 7310f54673efbc65f41b85debb2bef8d799ea07e

decouple device from document modules

reader.lua
@@ -19,6 +19,7 @@ local DataStorage = require("datastorage")
 pcall(dofile, DataStorage:getDataDir() .. "/defaults.persistent.lua")
 
 require("setupkoenv")
+Runtimectl = require("runtimectl")
 
 io.stdout:write(" [*] Version: ", require("version"):getCurrentRevision(), "\n\n")
 io.stdout:flush()
@@ -32,6 +33,23 @@ local lang_locale = G_reader_settings:readSetting("language")
 local _ = require("gettext")
 if lang_locale then
     _.changeLang(lang_locale)
+end
+
+-- setup various runtime control
+local Device = require("device")
+if Device:isAndroid() then
+    Runtimectl:restrictJIT()
+end
+
+if G_reader_settings:has("color_rendering") then
+    Runtimectl:setColorRenderingEnabled(G_reader_settings:isTrue("color_rendering"))
+else
+    Runtimectl:setColorRenderingEnabled(Device.screen.isColorScreen())
+end
+
+local dpi_override = G_reader_settings:readSetting("screen_dpi")
+if dpi_override ~= nil then
+    Device.screen:setDPI(dpi_override)
 end
 
 -- option parsing:
@@ -95,7 +113,6 @@ while argidx <= #ARGV do
 end
 
 local ConfirmBox = require("ui/widget/confirmbox")
-local Device = require("device")
 local Font = require("ui/font")
 local QuickStart = require("ui/quickstart")
 local UIManager = require("ui/uimanager")

frontend/cache.lua
@@ -7,7 +7,8 @@ local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
 local md5 = require("ffi/MD5")
 
-if require("device"):isAndroid() then
+local runtimectl = require("runtimectl")
+if runtimectl.should_restrict_JIT then
     require("jit").off(true, true)
 end
 

frontend/device/generic/device.lua
@@ -99,10 +99,6 @@ function Device:init()
     end
 
     self.screen.isColorScreen = self.hasColorScreen
-    self.screen.isColorEnabled = function()
-        if G_reader_settings:has("color_rendering") then return G_reader_settings:isTrue("color_rendering") end
-        return self.screen.isColorScreen()
-    end
 
     self.screen.isBGRFrameBuffer = self.hasBGRFrameBuffer
 

frontend/document/document.lua
@@ -5,7 +5,6 @@ local Configurable = require("configurable")
 local DrawContext = require("ffi/drawcontext")
 local Geom = require("ui/geometry")
 local Math = require("optmath")
-local Screen = require("device").screen
 local TileCacheItem = require("document/tilecacheitem")
 local lfs = require("libs/libkoreader-lfs")
 local logger = require("logger")
@@ -291,7 +290,7 @@ function Document:findText()
 end
 
 function Document:updateColorRendering()
-    if self.is_color_capable and Screen:isColorEnabled() then
+    if self.is_color_capable and Runtimectl.is_color_rendering_enabled then
         self.render_color = true
     else
         self.render_color = false

frontend/document/picdocument.lua
@@ -1,6 +1,6 @@
 local Document = require("document/document")
 local DrawContext = require("ffi/drawcontext")
-local Screen = require("device").screen
+local Runtimectl = require("runtimectl")
 local pic = nil
 
 local PicDocument = Document:new{
@@ -15,7 +15,7 @@ function PicDocument:init()
     self:updateColorRendering()
     if not pic then pic = require("ffi/pic") end
     -- pic.color needs to be true before opening document to allow toggling color
-    pic.color = Screen.isColorScreen()
+    pic.color = Runtimectl.is_color_rendering_enabled
     local ok
     ok, self._document = pcall(pic.openDocument, self.file)
     if not ok then

frontend/ui/elements/screen_color_menu_table.lua
@@ -1,14 +1,17 @@
 local Event = require("ui/event")
 local Screen = require("device").screen
 local UIManager = require("ui/uimanager")
+local Runtimectl = require("runtimectl")
 local _ = require("gettext")
 
 return {
     text = _("Color rendering"),
     enabled_func = Screen.isColorScreen,
-    checked_func = Screen.isColorEnabled,
+    checked_func = function() return Runtimectl.is_color_rendering_enabled end,
     callback = function()
-        G_reader_settings:saveSetting("color_rendering", not Screen.isColorEnabled())
+        Runtimectl:setColorRenderingEnabled(not Runtimectl.is_color_rendering_enabled)
+        G_reader_settings:saveSetting(
+            "color_rendering", Runtimectl.is_color_rendering_enabled)
         UIManager:broadcastEvent(Event:new("ColorRenderingUpdate"))
     end
 }

frontend/ui/rendertext.lua
@@ -8,7 +8,8 @@ local CacheItem = require("cacheitem")
 local BlitBuffer = require("ffi/blitbuffer")
 local logger = require("logger")
 
-if require("device"):isAndroid() then
+local runtimectl = require("runtimectl")
+if runtimectl.should_restrict_JIT then
     require("jit").off(true, true)
 end
 

frontend/ui/widget/container/inputcontainer.lua
@@ -33,7 +33,8 @@ local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
 local Screen = require("device").screen
 
-if require("device"):isAndroid() then
+local runtimectl = require("runtimectl")
+if runtimectl.should_restrict_JIT then
     require("jit").off(true, true)
 end
 

commit 84d1fa09e1e818753ed0896a9bbdc3a61ff8b584

Fix debug logging in the framebuffer module
Regression since 7310f54673efbc65f41b85debb2bef8d799ea07e

reader.lua
@@ -52,14 +52,11 @@ else
     end
 end
 
-local Device = require("device")
-local dpi_override = G_reader_settings:readSetting("screen_dpi")
-if dpi_override ~= nil then
-    Device:setScreenDPI(dpi_override)
-end
-
-local CanvasContext = require("document/canvascontext")
-CanvasContext:init(Device)
+-- should check DEBUG option in arg and turn on DEBUG before loading other
+-- modules, otherwise DEBUG in some modules may not be printed.
+local dbg = require("dbg")
+if G_reader_settings:isTrue("debug") then dbg:turnOn() end
+if G_reader_settings:isTrue("debug") and G_reader_settings:isTrue("debug_verbose") then dbg:setVerbose(true) end
 
 -- option parsing:
 local longopts = {
@@ -85,12 +82,6 @@ local function showusage()
     print("This software is licensed under the AGPLv3.")
     print("See http://github.com/koreader/koreader for more info.")
 end
-
--- should check DEBUG option in arg and turn on DEBUG before loading other
--- modules, otherwise DEBUG in some modules may not be printed.
-local dbg = require("dbg")
-if G_reader_settings:isTrue("debug") then dbg:turnOn() end
-if G_reader_settings:isTrue("debug") and G_reader_settings:isTrue("debug_verbose") then dbg:setVerbose(true) end
 
 local Profiler = nil
 local ARGV = arg
@@ -120,6 +111,15 @@ while argidx <= #ARGV do
         break
     end
 end
+
+local Device = require("device")
+local dpi_override = G_reader_settings:readSetting("screen_dpi")
+if dpi_override ~= nil then
+    Device:setScreenDPI(dpi_override)
+end
+
+local CanvasContext = require("document/canvascontext")
+CanvasContext:init(Device)
 
 local ConfirmBox = require("ui/widget/confirmbox")
 local QuickStart = require("ui/quickstart")