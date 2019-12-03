0xstillb/koreader-thai

commit ff32d44750cae754c59c6867cbdcc2b8eb47e62d

[chore] Extract Cyrillic/Russian keyboard (#5610)
Follow-up to #5583.

* Add separate Cyrillic/Russian keyboard

* And remove that from English

* Update French keyboard (is easy)

* Remove yahzhert, that was just a double of QWERTY

* Update Spanish keyboard

* Reduce Japanese to 4 layers and add globe

* Reduce Korean to 4 layers

* Reduce Greek to 8 layers

frontend/ui/data/keyboardlayouts/ru_keyboard.lua
@@ -1,0 +1,75 @@
+local en_popup = require("ui/data/keyboardlayouts/keypopup/en_popup")
+local com = en_popup.com -- comma (,)
+local prd = en_popup.prd -- period (.)
+local _at = en_popup._at
+local _eq = en_popup._eq -- equals sign (=)
+
+return {
+    min_layer = 1,
+    max_layer = 8,
+    shiftmode_keys = {[""] = true},
+    symbolmode_keys = {["Sym"] = true, ["ABC"] = true},
+    utf8mode_keys = {["🌐"] = true},
+    umlautmode_keys = {["Äéß"] = true},
+    keys = {
+        -- first row
+        {  --  1       2       3       4       5       6       7       8
+            { "Й",    "й",    "„",    "0",    "?",    "!",    "1",    "ª", },
+            { "Ц",    "ц",    "!",    "1",    "(",    "1",    "2",    "º", },
+            { "У",    "у",    _at,    "2",    ")",    "2",    "3",    "¡", },
+            { "К",    "к",    "#",    "3",    "~",    "3",    "4",    "¿", },
+            { "Е",    "е",     "+",    _eq,   "Ә",    "ә",    "5",    "¼", },
+            { "Н",    "н",    "€",    "(",    "І",    "і",    "6",    "½", },
+            { "Г",    "г",    "‰",    ")",    "Ң",    "ң",    "7",    "¾", },
+            { "Ш",    "ш",    "|",   "\\",    "Ғ",    "ғ",    "8",    "©", },
+            { "Щ",    "щ",    "?",    "/",    "Х",    "х",    "9",    "®", },
+            { "З",    "з",    "~",    "`",    "Ъ",    "ъ",    "0",    "™", },
+        },
+        -- second row
+        {  --  1       2       3       4       5       6       7       8
+            { "Ф",    "ф",    "…",    _at,    "*",    "0",    "Ş",    "ş", },
+            { "Ы",    "ы",    "$",    "4",    "+",    "4",    "İ",    "ı", },
+            { "В",    "в",    "%",    "5",    "-",    "5",    "Ğ",    "ğ", },
+            { "А",    "а",    "^",    "6",    _eq,    "6",    "Ć",    "ć", },
+            { "П",    "п",    ":",    ";",    "Ү",    "ү",    "Č",    "č", },
+            { "Р",    "р",    '"',    "'",    "Ұ",    "ұ",    "Đ",    "đ", },
+            { "О",    "о",    "{",    "[",    "Қ",    "қ",    "Š",    "š", },
+            { "Л",    "л",    "}",    "]",    "Ж",    "ж",    "Ž",    "ž", },
+            { "Д",    "д",    "_",    "-",    "Э",    "э",    "Ő",    "ő", },
+        },
+        -- third row
+        {  --  1       2       3       4       5       6       7       8
+            { label = "",
+              width = 1.5
+            },
+            { "Я",    "я",    "&",    "7",    ":",    "7",    "Ű",    "ű", },
+            { "Ч",    "ч",    "*",    "8",    ";",    "8",    "Ã",    "ã", },
+            { "С",    "с",    "£",    "9",    "'",    "9",    "Þ",    "þ", },
+            { "М",    "м",    "<",    com,    "Ө",    "ө",    "Ý",    "ý", },
+            { "И",    "и",    ">",    prd,    "Һ",    "һ",    "†",    "‡", },
+            { "Т",    "т",    "‘",    "↑",    "Б",    "б",    "–",    "—", },
+            { "Ь",    "ь",    "’",    "↓",    "Ю",    "ю",    "…",    "¨", },
+            { label = "Backspace",
+              icon = "resources/icons/appbar.clear.reflect.horizontal.png",
+              width = 1.5
+            },
+        },
+        -- fourth row
+        {
+            { "Sym",  "Sym",  "ABC",  "ABC",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
+              width = 1.5},
+            { label = "🌐", },
+            { "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
+            { label = "space",
+              " ",        " ",    " ",    " ",   " ",    " ",    " ",    " ",    " ",    " ",
+              width = 3.0},
+            { com,    com,    "“",    "←",    "Ё",    "ё",    "Ũ",   "ũ",    com,    com, },
+            { prd,    prd,    "”",    "→",    prd,    prd,    "Ĩ",   "ĩ",    prd,    prd, },
+            { label = "Enter",
+              "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
+              icon = "resources/icons/appbar.arrow.enter.png",
+              width = 1.5,
+            },
+        },
+    },
+}

frontend/ui/data/keyboardlayouts/el_keyboard.lua
@@ -3,58 +3,6 @@ local com = en_popup.com -- comma (,)
 local prd = en_popup.prd -- period (.)
 local _at = en_popup._at
 local _eq = en_popup._eq -- equals sign (=)
-local _A_ = en_popup._A_
-local _a_ = en_popup._a_
-local _B_ = en_popup._B_
-local _b_ = en_popup._b_
-local _C_ = en_popup._C_
-local _c_ = en_popup._c_
-local _D_ = en_popup._D_
-local _d_ = en_popup._d_
-local _E_ = en_popup._E_
-local _e_ = en_popup._e_
-local _F_ = en_popup._F_
-local _f_ = en_popup._f_
-local _G_ = en_popup._G_
-local _g_ = en_popup._g_
-local _H_ = en_popup._H_
-local _h_ = en_popup._h_
-local _I_ = en_popup._I_
-local _i_ = en_popup._i_
-local _J_ = en_popup._J_
-local _j_ = en_popup._j_
-local _K_ = en_popup._K_
-local _k_ = en_popup._k_
-local _L_ = en_popup._L_
-local _l_ = en_popup._l_
-local _M_ = en_popup._M_
-local _m_ = en_popup._m_
-local _N_ = en_popup._N_
-local _n_ = en_popup._n_
-local _O_ = en_popup._O_
-local _o_ = en_popup._o_
-local _P_ = en_popup._P_
-local _p_ = en_popup._p_
-local _Q_ = en_popup._Q_
-local _q_ = en_popup._q_
-local _R_ = en_popup._R_
-local _r_ = en_popup._r_
-local _S_ = en_popup._S_
-local _s_ = en_popup._s_
-local _T_ = en_popup._T_
-local _t_ = en_popup._t_
-local _U_ = en_popup._U_
-local _u_ = en_popup._u_
-local _V_ = en_popup._V_
-local _v_ = en_popup._v_
-local _W_ = en_popup._W_
-local _w_ = en_popup._w_
-local _X_ = en_popup._X_
-local _x_ = en_popup._x_
-local _Y_ = en_popup._Y_
-local _y_ = en_popup._y_
-local _Z_ = en_popup._Z_
-local _z_ = en_popup._z_
 
 local el_popup = require("ui/data/keyboardlayouts/keypopup/el_popup")
 local _A_el = el_popup._A_el
@@ -112,49 +60,49 @@ local _z_el = el_popup._z_el
 
 return {
     min_layer = 1,
-    max_layer = 12,
+    max_layer = 8,
     shiftmode_keys = {[""] = true},
     symbolmode_keys = {["Sym"] = true, ["ABC"] = true},
     utf8mode_keys = {["🌐"] = true},
     umlautmode_keys = {["Äéß"] = true},
     keys = {
         -- first row
-        {  --  1       2       3       4       5       6       7       8       9       10      11      12
-            { ":",    ";",    "„",    "0",    _Q_,    _q_,    "1",    "!",    "Ä",    "ä",    "1",    "ª", },
-            { "|",    "ς",    "!",    "1",    _W_,    _w_,    "2",    "?",    "Ö",    "ö",    "2",    "º", },
-            { _E_el,  _e_el,  _at,    "2",    _E_,    _e_,    "Ü",    "ü",    "Έ",    "έ",    "3",    "¡", },
-            { _R_el,  _r_el,  "#",    "3",    _R_,    _r_,    "4",    ";",    "ß",    "ß",    "4",    "¿", },
-            { _T_el,  _t_el,  "+",    _eq,    _T_,    _t_,    "5",    ":",    "À",    "à",    "5",    "¼", },
-            { _Y_el,  _y_el,  "€",    "(",    _Y_,    _y_,    "Â",    "â",    "Ύ",    "ύ",    "6",    "½", },
-            { _U_el,  _u_el,  "‰",    ")",    _U_,    _u_,    "7",    "(",    "Æ",    "æ",    "7",    "¾", },
-            { _I_el,  _i_el,  "|",    "\\",   _I_,    _i_,    "Ç",    "ç",    "Ί",    "ί",    "8",    "©", },
-            { _O_el,  _o_el,  "?",    "/",    _O_,    _o_,    "9",    "è",    "Ό",    "ό",    "9",    "®", },
-            { _P_el,  _p_el,  "~",    "`",    _P_,    _p_,    "È",    ")",    "É",    "é",    "0",    "™", },
+        {  --  1       2       3       4       5       6       7       8
+            { ":",    ";",    "„",    "0",    "Ä",    "ä",    "1",    "ª", },
+            { "|",    "ς",    "!",    "1",    "Ö",    "ö",    "2",    "º", },
+            { _E_el,  _e_el,  _at,    "2",    "Έ",    "έ",    "3",    "¡", },
+            { _R_el,  _r_el,  "#",    "3",    "ß",    "ß",    "4",    "¿", },
+            { _T_el,  _t_el,  "+",    _eq,    "À",    "à",    "5",    "¼", },
+            { _Y_el,  _y_el,  "€",    "(",    "Ύ",    "ύ",    "6",    "½", },
+            { _U_el,  _u_el,  "‰",    ")",    "Æ",    "æ",    "7",    "¾", },
+            { _I_el,  _i_el,  "|",    "\\",   "Ί",    "ί",    "8",    "©", },
+            { _O_el,  _o_el,  "?",    "/",    "Ό",    "ό",    "9",    "®", },
+            { _P_el,  _p_el,  "~",    "`",    "É",    "é",    "0",    "™", },
         },
         -- second row
-        {  --  1       2       3       4       5       6       7       8       9       10      11      12
-            { _A_el,  _a_el,  "…",    _at,    _A_,    _a_,    "Ê",    "ê",    "Ά",    "ά",    "Ş",    "ş", },
-            { _S_el,  _s_el,  "$",    "4",    _S_,    _s_,    "Ѕ",    "ѕ",    "Ë",    "ë",    "İ",    "ı", },
-            { _D_el,  _d_el,  "%",    "5",    _D_,    _d_,    "Э",    "«",    "Î",    "î",    "Ğ",    "ğ", },
-            { _F_el,  _f_el,  "^",    "6",    _F_,    _f_,    _eq,    "»",    "Ï",    "ï",    "Ć",    "ć", },
-            { _G_el,  _g_el,  ":",    ";",    _G_,    _g_,    "Ґ",    "ґ",    "Ô",    "ô",    "Č",    "č", },
-            { _H_el,  _h_el,  '"',    "'",    _H_,    _h_,    "Œ",    "œ",    "Ή",    "ή",    "Đ",    "đ", },
-            { _J_el,  _j_el,  "{",    "[",    _J_,    _j_,    "Ù",    "ù",    "Ί",    "ί",    "Š",    "š", },
-            { _K_el,  _k_el,  "}",    "]",    _K_,    _k_,    "Ќ",    "ќ",    "Û",    "û",    "Ž",    "ž", },
-            { _L_el,  _l_el,  "_",    "-",    _L_,    _l_,    "Љ",    "љ",    "Ÿ",    "ÿ",    "Ő",    "ő", },
+        {  --  1       2       3       4       5       6       7       8
+            { _A_el,  _a_el,  "…",    _at,    "Ά",    "ά",    "Ş",    "ş", },
+            { _S_el,  _s_el,  "$",    "4",    "Ë",    "ë",    "İ",    "ı", },
+            { _D_el,  _d_el,  "%",    "5",    "Î",    "î",    "Ğ",    "ğ", },
+            { _F_el,  _f_el,  "^",    "6",    "Ï",    "ï",    "Ć",    "ć", },
+            { _G_el,  _g_el,  ":",    ";",    "Ô",    "ô",    "Č",    "č", },
+            { _H_el,  _h_el,  '"',    "'",    "Ή",    "ή",    "Đ",    "đ", },
+            { _J_el,  _j_el,  "{",    "[",    "Ί",    "ί",    "Š",    "š", },
+            { _K_el,  _k_el,  "}",    "]",    "Û",    "û",    "Ž",    "ž", },
+            { _L_el,  _l_el,  "_",    "-",    "Ÿ",    "ÿ",    "Ő",    "ő", },
         },
         -- third row
-        {  --  1       2       3       4       5       6       7       8       9       10      11      12
+        {  --  1       2       3       4       5       6       7       8
             { label = "",
               width = 1.5
             },
-            { _Z_el,  _z_el,  "&",    "7",    _Z_,    _z_,    "Щ",    "щ",    "Á",    "á",    "Ű",    "ű", },
-            { _X_el,  _x_el,  "*",    "8",    _X_,    _x_,    "№",    "@",    "É",    "é",    "Ø",    "ø", },
-            { _C_el,  _c_el,  "£",    "9",    _C_,    _c_,    "Џ",    "џ",    "Í",    "í",    "Þ",    "þ", },
-            { _V_el,  _v_el,  "<",    com,    _V_,    _v_,    "Ñ",    "ñ",    "Ώ",    "ώ",    "Ý",    "ý", },
-            { _B_el,  _b_el,  ">",    prd,    _B_,    _b_,    "Ћ",    "ћ",    "Ó",    "ó",    "†",    "‡", },
-            { _N_el,  _n_el,  "‘",    "↑",    _N_,    _n_,    "Њ",    "њ",    "Ú",    "ú",    "–",    "—", },
-            { _M_el,  _m_el,  "’",    "↓",    _M_,    _m_,    "Ї",    "ї",    "Ç",    "ç",    "…",    "¨", },
+            { _Z_el,  _z_el,  "&",    "7",    "Á",    "á",    "Ű",    "ű", },
+            { _X_el,  _x_el,  "*",    "8",    "É",    "é",    "Ø",    "ø", },
+            { _C_el,  _c_el,  "£",    "9",    "Í",    "í",    "Þ",    "þ", },
+            { _V_el,  _v_el,  "<",    com,    "Ώ",    "ώ",    "Ý",    "ý", },
+            { _B_el,  _b_el,  ">",    prd,    "Ó",    "ó",    "†",    "‡", },
+            { _N_el,  _n_el,  "‘",    "↑",    "Ú",    "ú",    "–",    "—", },
+            { _M_el,  _m_el,  "’",    "↓",    "Ç",    "ç",    "…",    "¨", },
             { label = "Backspace",
               icon = "resources/icons/appbar.clear.reflect.horizontal.png",
               width = 1.5
@@ -162,17 +110,17 @@ return {
         },
         -- fourth row
         {
-            { "Sym",     "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
+            { "Sym",  "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
               width = 1.5},
             { label = "🌐", },
-            { "Äéß",     "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
+            { "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
             { label = "space",
-              " ",        " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",
+              " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",
               width = 3.0},
-            { com,        com,    "“",    "←",    com,    com,    "Ё",    "ё",    "Ũ",   "ũ",    com,    com, },
-            { prd,        prd,    "”",    "→",    prd,    prd,    prd,    prd,    "Ĩ",   "ĩ",    prd,    prd, },
+            { com,    com,    "“",    "←",    "Ũ",   "ũ",    com,    com, },
+            { prd,    prd,    "”",    "→",    "Ĩ",   "ĩ",    prd,    prd, },
             { label = "Enter",
-              "\n",       "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
+              "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
               icon = "resources/icons/appbar.arrow.enter.png",
               width = 1.5,
             },

frontend/ui/data/keyboardlayouts/en_keyboard.lua
@@ -58,49 +58,49 @@ local _z_ = en_popup._z_
 
 return {
     min_layer = 1,
-    max_layer = 12,
+    max_layer = 8,
     shiftmode_keys = {[""] = true},
     symbolmode_keys = {["Sym"] = true, ["ABC"] = true},
     utf8mode_keys = {["🌐"] = true},
     umlautmode_keys = {["Äéß"] = true},
     keys = {
         -- first row
-        {  --  1           2       3       4       5       6       7       8       9       10      11      12
-            { _Q_,        _q_,    "„",    "0",    "Й",    "й",    "?",    "!",    "Å",    "å",    "1",    "ª", },
-            { _W_,        _w_,    "!",    "1",    "Ц",    "ц",    "(",    "1",    "Ä",    "ä",    "2",    "º", },
-            { _E_,        _e_,    _at,    "2",    "У",    "у",    ")",    "2",    "Ö",    "ö",    "3",    "¡", },
-            { _R_,        _r_,    "#",    "3",    "К",    "к",    "~",    "3",    "ß",    "ß",    "4",    "¿", },
-            { _T_,        _t_,    "+",    _eq,    "Е",    "е",    "Ә",    "ә",    "À",    "à",    "5",    "¼", },
-            { _Y_,        _y_,    "€",    "(",    "Н",    "н",    "І",    "і",    "Â",    "â",    "6",    "½", },
-            { _U_,        _u_,    "‰",    ")",    "Г",    "г",    "Ң",    "ң",    "Æ",    "æ",    "7",    "¾", },
-            { _I_,        _i_,    "|",   "\\",    "Ш",    "ш",    "Ғ",    "ғ",    "Ü",    "ü",    "8",    "©", },
-            { _O_,        _o_,    "?",    "/",    "Щ",    "щ",    "Х",    "х",    "È",    "è",    "9",    "®", },
-            { _P_,        _p_,    "~",    "`",    "З",    "з",    "Ъ",    "ъ",    "É",    "é",    "0",    "™", },
+        {  --  1       2       3       4       5       6       7       8
+            { _Q_,    _q_,    "„",    "0",    "Å",    "å",    "1",    "ª", },
+            { _W_,    _w_,    "!",    "1",    "Ä",    "ä",    "2",    "º", },
+            { _E_,    _e_,    _at,    "2",    "Ö",    "ö",    "3",    "¡", },
+            { _R_,    _r_,    "#",    "3",    "ß",    "ß",    "4",    "¿", },
+            { _T_,    _t_,    "+",    _eq,    "À",    "à",    "5",    "¼", },
+            { _Y_,    _y_,    "€",    "(",    "Â",    "â",    "6",    "½", },
+            { _U_,    _u_,    "‰",    ")",    "Æ",    "æ",    "7",    "¾", },
+            { _I_,    _i_,    "|",   "\\",    "Ü",    "ü",    "8",    "©", },
+            { _O_,    _o_,    "?",    "/",    "È",    "è",    "9",    "®", },
+            { _P_,    _p_,    "~",    "`",    "É",    "é",    "0",    "™", },
         },
         -- second row
-        {  --  1           2       3       4       5       6       7       8       9       10      11      12
-            { _A_,        _a_,    "…",    _at,    "Ф",    "ф",    "*",    "0",    "Ê",    "ê",    "Ş",    "ş", },
-            { _S_,        _s_,    "$",    "4",    "Ы",    "ы",    "+",    "4",    "Ë",    "ë",    "İ",    "ı", },
-            { _D_,        _d_,    "%",    "5",    "В",    "в",    "-",    "5",    "Î",    "î",    "Ğ",    "ğ", },
-            { _F_,        _f_,    "^",    "6",    "А",    "а",    _eq,    "6",    "Ï",    "ï",    "Ć",    "ć", },
-            { _G_,        _g_,    ":",    ";",    "П",    "п",    "Ү",    "ү",    "Ô",    "ô",    "Č",    "č", },
-            { _H_,        _h_,    '"',    "'",    "Р",    "р",    "Ұ",    "ұ",    "Œ",    "œ",    "Đ",    "đ", },
-            { _J_,        _j_,    "{",    "[",    "О",    "о",    "Қ",    "қ",    "Ù",    "ù",    "Š",    "š", },
-            { _K_,        _k_,    "}",    "]",   "Л",    "л",    "Ж",    "ж",    "Û",    "û",    "Ž",    "ž", },
-            { _L_,        _l_,    "_",    "-",    "Д",    "д",    "Э",    "э",    "Ÿ",    "ÿ",    "Ő",    "ő", },
+        {  --  1       2       3       4       5       6       7       8
+            { _A_,    _a_,    "…",    _at,    "Ê",    "ê",    "Ş",    "ş", },
+            { _S_,    _s_,    "$",    "4",    "Ë",    "ë",    "İ",    "ı", },
+            { _D_,    _d_,    "%",    "5",    "Î",    "î",    "Ğ",    "ğ", },
+            { _F_,    _f_,    "^",    "6",    "Ï",    "ï",    "Ć",    "ć", },
+            { _G_,    _g_,    ":",    ";",    "Ô",    "ô",    "Č",    "č", },
+            { _H_,    _h_,    '"',    "'",    "Œ",    "œ",    "Đ",    "đ", },
+            { _J_,    _j_,    "{",    "[",    "Ù",    "ù",    "Š",    "š", },
+            { _K_,    _k_,    "}",    "]",    "Û",    "û",    "Ž",    "ž", },
+            { _L_,    _l_,    "_",    "-",    "Ÿ",    "ÿ",    "Ő",    "ő", },
         },
         -- third row
-        {  --  1           2       3       4       5       6       7       8       9       10      11      12
+        {  --  1       2       3       4       5       6       7       8
             { label = "",
               width = 1.5
             },
-            { _Z_,        _z_,    "&",    "7",    "Я",    "я",    ":",    "7",    "Á",    "á",    "Ű",    "ű", },
-            { _X_,        _x_,    "*",    "8",    "Ч",    "ч",    ";",    "8",    "Ø",    "ø",    "Ã",    "ã", },
-            { _C_,        _c_,    "£",    "9",    "С",    "с",    "'",    "9",    "Í",    "í",    "Þ",    "þ", },
-            { _V_,        _v_,    "<",    com,    "М",    "м",    "Ө",    "ө",    "Ñ",    "ñ",    "Ý",    "ý", },
-            { _B_,        _b_,    ">",    prd,    "И",    "и",    "Һ",    "һ",    "Ó",    "ó",    "†",    "‡", },
-            { _N_,        _n_,    "‘",    "↑",    "Т",    "т",    "Б",    "б",    "Ú",    "ú",    "–",    "—", },
-            { _M_,        _m_,    "’",    "↓",    "Ь",    "ь",    "Ю",    "ю",    "Ç",    "ç",    "…",    "¨", },
+            { _Z_,    _z_,    "&",    "7",    "Á",    "á",    "Ű",    "ű", },
+            { _X_,    _x_,    "*",    "8",    "Ø",    "ø",    "Ã",    "ã", },
+            { _C_,    _c_,    "£",    "9",    "Í",    "í",    "Þ",    "þ", },
+            { _V_,    _v_,    "<",    com,    "Ñ",    "ñ",    "Ý",    "ý", },
+            { _B_,    _b_,    ">",    prd,    "Ó",    "ó",    "†",    "‡", },
+            { _N_,    _n_,    "‘",    "↑",    "Ú",    "ú",    "–",    "—", },
+            { _M_,    _m_,    "’",    "↓",    "Ç",    "ç",    "…",    "¨", },
             { label = "Backspace",
               icon = "resources/icons/appbar.clear.reflect.horizontal.png",
               width = 1.5
@@ -108,17 +108,17 @@ return {
         },
         -- fourth row
         {
-            { "Sym",     "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
+            { "Sym",  "Sym",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
               width = 1.5},
             { label = "🌐", },
-            { "Äéß",     "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
+            { "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
             { label = "space",
-              " ",        " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",
+              " ",    " ",    " ",    " ",    " ",    " ",    " ",    " ",
               width = 3.0},
-            { com,        com,    "“",    "←",    com,    com,    "Ё",    "ё",    "Ũ",   "ũ",    com,    com, },
-            { prd,        prd,    "”",    "→",    prd,    prd,    prd,    prd,    "Ĩ",   "ĩ",    prd,    prd, },
+            { com,    com,    "“",    "←",    "Ũ",   "ũ",    com,    com, },
+            { prd,    prd,    "”",    "→",    "Ĩ",   "ĩ",    prd,    prd, },
             { label = "Enter",
-              "\n",       "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
+              "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
               icon = "resources/icons/appbar.arrow.enter.png",
               width = 1.5,
             },

frontend/ui/data/keyboardlayouts/es_keyboard.lua
@@ -5,8 +5,8 @@ local keys = es_keyboard.keys
 
 -- Insert an additional key at the end of 2nd row for easy Ñ and ñ
 table.insert(keys[2],
-           --  1           2       3       4       5       6       7       8       9       10      11      12
-            { "Ñ",        "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",  }
+           --  1       2       3       4       5       6       7       8
+            { "Ñ",    "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",    "Ñ",    "ñ",  }
 )
 
 -- Rename "space"

frontend/ui/data/keyboardlayouts/fr_keyboard.lua
@@ -11,8 +11,8 @@ keys[1][2][2], keys[3][2][2] = keys[3][2][2], keys[1][2][2] -- w <> z
 
 -- Insert an additional key at the end of 2nd row for M
 table.insert(keys[2],
-           --  1           2       3       4       5       6       7       8       9       10      11      12
-            { "M",        "m",    "§",    "+",    "Д",    "д",    "Э",    "э",    "Œ",    "œ",    "Ő",    "ő", }
+           --  1       2       3       4       5       6       7       8
+            { "M",    "m",    "§",    "+",    "Œ",    "œ",    "Ő",    "ő", }
 )
 -- And swap the english M on the 3rd row to ','
 keys[3][8][1] = ","

commit 9d0f5c951a621fc828a23b2560d173a6204954aa

[UX] ru keyboard ё popup and translation (#5662)
Changes in this commit:
- Fix arrays to large (10 elements instead of 8) copied from the old keyboard system.  Issue introduced in #5610 
- Add ё popup.
- Change shift key as per #5659 (for both ro and ru keyboards)
- Translate Russian space and symbol keys.
- Rename Sym key to 123

frontend/ui/data/keyboardlayouts/keypopup/ru_popup.lua
@@ -7,4 +7,12 @@ return {
         "ж",
         north = "ӂ", -- ж with breve (Moldavian)
     },
+    _Ye_ = {
+        "Е",
+        north = "Ё",
+    },
+    _ye_ = {
+        "е",
+        north = "ё",
+    },
 }

frontend/ui/data/keyboardlayouts/ro_keyboard.lua
@@ -18,7 +18,7 @@ local _u_ = ro_popup._u_
 return {
     min_layer = 1,
     max_layer = 8,
-    shiftmode_keys = {[""] = true},
+    shiftmode_keys = {[""] = true, ["1/2"] = true, ["2/2"] = true},
     symbolmode_keys = {["123"] = true, ["ABC"] = true, ["alt"] = true},
     utf8mode_keys = {["🌐"] = true},
     umlautmode_keys = {["Îșț"] = true},
@@ -50,7 +50,7 @@ return {
         },
         -- third row
         {  --  1       2       3       4       5       6       7       8
-            { label = "",
+            { "",   "",   "2/2",  "1/2",   "",   "",   "",    "",
               width = 1.5
             },
             { "Z",    "z",    "&",    "7",    "Ѣ",    "ѣ",    "Ű",    "ű", },

frontend/ui/data/keyboardlayouts/ru_keyboard.lua
@@ -6,14 +6,16 @@ local _at = en_popup._at
 local _eq = en_popup._eq -- equals sign (=)
 local _Je_ = ru_popup._Je_
 local _je_ = ru_popup._je_
+local _Ye_ = ru_popup._Ye_
+local _ye_ = ru_popup._ye_
 
 return {
     min_layer = 1,
     max_layer = 8,
-    shiftmode_keys = {[""] = true},
-    symbolmode_keys = {["Sym"] = true, ["ABC"] = true},
+    shiftmode_keys = {[""] = true, ["1/2"] = true, ["2/2"] = true},
+    symbolmode_keys = {["123"] = true, ["АБВ"] = true, ["ещё"] = true},
     utf8mode_keys = {["🌐"] = true},
-    umlautmode_keys = {["Äéß"] = true},
+    umlautmode_keys = {["Жбъ"] = true},
     keys = {
         -- first row
         {  --  1       2       3       4       5       6       7       8
@@ -21,7 +23,7 @@ return {
             { "Ц",    "ц",    "!",    "1",    "(",    "1",    "2",    "º", },
             { "У",    "у",    _at,    "2",    ")",    "2",    "3",    "¡", },
             { "К",    "к",    "#",    "3",    "~",    "3",    "4",    "¿", },
-            { "Е",    "е",     "+",    _eq,   "Ә",    "ә",    "5",    "¼", },
+            { _Ye_,  _ye_,    "+",    _eq,    "Ә",    "ә",    "5",    "¼", },
             { "Н",    "н",    "€",    "(",    "І",    "і",    "6",    "½", },
             { "Г",    "г",    "‰",    ")",    "Ң",    "ң",    "7",    "¾", },
             { "Ш",    "ш",    "|",   "\\",    "Ғ",    "ғ",    "8",    "©", },
@@ -30,19 +32,19 @@ return {
         },
         -- second row
         {  --  1       2       3       4       5       6       7       8
-            { "Ф",    "ф",    "…",    _at,    "*",    "0",    "Ş",    "ş", },
-            { "Ы",    "ы",    "$",    "4",    "+",    "4",    "İ",    "ı", },
+            { "Ф",    "ф",    "…",    _at,    "*",    "0",    "«",    "«", },
+            { "Ы",    "ы",    "$",    "4",    "+",    "4",    "»",    "»", },
             { "В",    "в",    "%",    "5",    "-",    "5",    "Ğ",    "ğ", },
             { "А",    "а",    "^",    "6",    _eq,    "6",    "Ć",    "ć", },
             { "П",    "п",    ":",    ";",    "Ү",    "ү",    "Č",    "č", },
             { "Р",    "р",    '"',    "'",    "Ұ",    "ұ",    "Đ",    "đ", },
             { "О",    "о",    "{",    "[",    "Қ",    "қ",    "Š",    "š", },
             { "Л",    "л",    "}",    "]",   _Je_,   _je_,    "Ž",    "ž", },
-            { "Д",    "д",    "_",    "-",    "Э",    "э",    "Ő",    "ő", },
+            { "Д",    "д",    "_",    "-",    "Э",    "э",    "§",    "§", },
         },
         -- third row
         {  --  1       2       3       4       5       6       7       8
-            { label = "",
+            { "",   "",   "2/2",  "1/2",   "",   "",   "",    "",
               width = 1.5
             },
             { "Я",    "я",    "&",    "7",    ":",    "7",    "Ű",    "ű", },
@@ -59,17 +61,17 @@ return {
         },
         -- fourth row
         {
-            { "Sym",  "Sym",  "ABC",  "ABC",  "ABC",  "ABC",  "Sym",  "Sym",  "ABC",  "ABC",
+            { "123",  "123",  "АБВ",  "АБВ",  "ещё",  "ещё",  "АБВ",  "АБВ",
               width = 1.5},
             { label = "🌐", },
-            { "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß",  "Äéß", },
-            { label = "space",
-              " ",        " ",    " ",    " ",   " ",    " ",    " ",    " ",    " ",    " ",
+            { "Жбъ",  "Жбъ",  "Жбъ",  "Жбъ",  "Жбъ",  "Жбъ",  "Жбъ",  "Жбъ", },
+            { label = "пробел",
+              " ",        " ",    " ",    " ",   " ",    " ",    " ",    " ",
               width = 3.0},
-            { com,    com,    "“",    "←",    "Ё",    "ё",    "Ũ",   "ũ",    com,    com, },
-            { prd,    prd,    "”",    "→",    prd,    prd,    "Ĩ",   "ĩ",    prd,    prd, },
+            { com,    com,    "“",    "←",    "Ё",    "ё",    com,    com, },
+            { prd,    prd,    "”",    "→",    prd,    prd,    prd,    prd, },
             { label = "⮠",
-              "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
+              "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",   "\n",
               width = 1.5,
               bold = true
             },