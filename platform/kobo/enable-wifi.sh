0xstillb/koreader-thai

commit da336192a8a9b16184a724ace993d775e213a9e3

Set the WiFi regulatory domain properly on the Elipsa
No idea how restrictive the defaults are, but stuff is weird in the 5G
band, so, do it right.

platform/kobo/enable-wifi.sh
@@ -33,11 +33,23 @@ fi
 usleep 250000
 # NOTE: Used to be exported in WIFI_MODULE_PATH before FW 4.23
 if ! grep -q "${WIFI_MODULE}" "/proc/modules"; then
+    # Set the Wi-Fi regulatory domain properly if necessary...
+    WIFI_COUNTRY_CODE_PARM=""
+    if grep -q "^WifiRegulatoryDomain=" "/mnt/onboard/.kobo/Kobo/Kobo eReader.conf"; then
+        WIFI_COUNTRY_CODE="$(grep "^WifiRegulatoryDomain=" "/mnt/onboard/.kobo/Kobo/Kobo eReader.conf" | cut -d '=' -f2)"
+
+        case "${WIFI_MODULE}" in
+            "8821cs")
+                WIFI_COUNTRY_CODE_PARM="rtw_country_code=${WIFI_COUNTRY_CODE}"
+                ;;
+        esac
+    fi
+
     if [ -e "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko" ]; then
-        insmod "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko"
+        insmod "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
     elif [ -e "/drivers/${PLATFORM}/${WIFI_MODULE}.ko" ]; then
         # NOTE: Modules are unsorted on Mk. 8
-        insmod "/drivers/${PLATFORM}/${WIFI_MODULE}.ko"
+        insmod "/drivers/${PLATFORM}/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
     fi
 fi
 # Race-y as hell, don't try to optimize this!

commit 4c81e9cea1ab02086aaa7c3ef34829134fc79a4d

Unbreak Wi-Fi on < Mk.8 (#8077)
Don't pass empty parameters to insmod, it apparently doesn't like it...

Fix #8075
Regression since da336192a8a9b16184a724ace993d775e213a9e3

platform/kobo/enable-wifi.sh
@@ -46,10 +46,18 @@ if ! grep -q "${WIFI_MODULE}" "/proc/modules"; then
     fi
 
     if [ -e "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko" ]; then
-        insmod "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
+        if [ -n "${WIFI_COUNTRY_CODE_PARM}" ]; then
+            insmod "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
+        else
+            insmod "/drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko"
+        fi
     elif [ -e "/drivers/${PLATFORM}/${WIFI_MODULE}.ko" ]; then
         # NOTE: Modules are unsorted on Mk. 8
-        insmod "/drivers/${PLATFORM}/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
+        if [ -n "${WIFI_COUNTRY_CODE_PARM}" ]; then
+            insmod "/drivers/${PLATFORM}/${WIFI_MODULE}.ko" "${WIFI_COUNTRY_CODE_PARM}"
+        else
+            insmod "/drivers/${PLATFORM}/${WIFI_MODULE}.ko"
+        fi
     fi
 fi
 # Race-y as hell, don't try to optimize this!