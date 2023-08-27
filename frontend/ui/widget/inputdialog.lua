0xstillb/koreader-thai

commit 653ed059d448c8dac030204f7babfc393977d5c1

InputText: Actually flip the is_keyboard_hidden flag properly
MultiInputDialog does a close -> show in the same callback,
so if we don't actually keep it in sync with the actual state, we lose
the keyboard and essentially softlock the UI, which is Very Bad(TM).

NOTE: InputDialog has its own keyboard_hidden flag, and it looks...
fairly nightmarish.

frontend/ui/widget/inputtext.lua
@@ -64,7 +64,7 @@ local InputText = InputContainer:extend{
     for_measurement_only = nil, -- When the widget is a one-off used to compute text height
     do_select = false, -- to start text selection
     selection_start_pos = nil, -- selection start position
-    is_keyboard_hidden = false, -- to be able to show the keyboard again when it was hidden
+    is_keyboard_hidden = true, -- to be able to show the keyboard again when it was hidden. (On init, it's the caller's responsibility to call onShowKeyboard, as far as we're concerned, it's hidden)
 }
 
 -- These may be (internally) overloaded as needed, depending on Device capabilities.
@@ -132,9 +132,8 @@ local function initTouchEvents()
             if self.parent.onSwitchFocus then
                 self.parent:onSwitchFocus(self)
             else
-                if self.is_keyboard_hidden == true then
+                if self.is_keyboard_hidden then
                     self:onShowKeyboard()
-                    self.is_keyboard_hidden = false
                 end
             end
             -- zh keyboard with candidates shown here has _frame_textwidget.dimen = nil.
@@ -677,8 +676,12 @@ dbg:guard(InputText, "onTextInput",
 
 function InputText:onShowKeyboard(ignore_first_hold_release)
     Device:startTextInput()
-    self.keyboard.ignore_first_hold_release = ignore_first_hold_release
-    UIManager:show(self.keyboard)
+
+    if self.is_keyboard_hidden then
+        self.keyboard.ignore_first_hold_release = ignore_first_hold_release
+        UIManager:show(self.keyboard)
+        self.is_keyboard_hidden = false
+    end
     return true
 end
 

commit ada5f7c819a108203093596b91cdb7b97f0f2b1f

InputText/InputDialog: fix keyboard issue (#10850)
Fix issue introduced by 653ed059: with full screen
text editor and hiding the keyboard, a tap in the
text area would show a new keyboard hiding the
buttons allowing to save/close the text editor,
getting us stuck there.

frontend/ui/widget/inputdialog.lua
@@ -368,6 +368,7 @@ function InputDialog:init()
         scroll_by_pan = self.scroll_by_pan,
         cursor_at_end = self.cursor_at_end,
         readonly = self.readonly,
+        manage_keyboard_state = not self.add_nav_bar, -- we handle keyboard toggle ourselve if nav_bar
         parent = self,
         is_text_edited = self._text_modified,
         top_line_num = self._top_line_num,

frontend/ui/widget/inputtext.lua
@@ -34,6 +34,7 @@ local InputText = InputContainer:extend{
     edit_callback = nil, -- called with true when text modified, false on init or text re-set
     scroll_callback = nil, -- called with (low, high) when view is scrolled (cf ScrollTextWidget)
     scroll_by_pan = false, -- allow scrolling by lines with Pan (needs scroll=true)
+    manage_keyboard_state = true, -- manage keyboard hidden/shown state
 
     width = nil,
     height = nil, -- when nil, will be set to original text height (possibly
@@ -132,7 +133,7 @@ local function initTouchEvents()
             if self.parent.onSwitchFocus then
                 self.parent:onSwitchFocus(self)
             else
-                if self.is_keyboard_hidden then
+                if self.is_keyboard_hidden and self.manage_keyboard_state then
                     self:onShowKeyboard()
                 end
             end
@@ -677,7 +678,7 @@ dbg:guard(InputText, "onTextInput",
 function InputText:onShowKeyboard(ignore_first_hold_release)
     Device:startTextInput()
 
-    if self.is_keyboard_hidden then
+    if self.is_keyboard_hidden or not self.manage_keyboard_state then
         self.keyboard.ignore_first_hold_release = ignore_first_hold_release
         UIManager:show(self.keyboard)
         self.is_keyboard_hidden = false
@@ -688,7 +689,7 @@ end
 function InputText:onCloseKeyboard()
     Device:stopTextInput()
 
-    if not self.is_keyboard_hidden then
+    if not self.is_keyboard_hidden or not self.manage_keyboard_state then
         UIManager:close(self.keyboard)
         self.is_keyboard_hidden = true
     end